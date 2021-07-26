a83de777f6abe6c5e11130448662d85d4bd4b577
Chomchanok <chomchanok_y@cmu.ac.th>
2021-07-26 20:07:20 +0700

    nearly complete

--- HW03A3.py
+++ HW03A3.py
@@ -1,23 +1,26 @@
 import random
-status = 0  # check game status, 0: normal, 1: abnormal
 stick = int(input("How many sticks (N) in the pile:"))
 print('There are {} sticks in the pile'.format(stick))
 name = input('What is your name :')
-turn = 1  # 0 user play first, 1 bot play first
+turn = random.randint(0, 1)  # 0 user play first, 1 bot play first
 if turn == 0:
     print("####################\n{} play first\n####################".format(name))
 else:
     print("###################\n Bot play first\n###################")
-
 while (stick > 0):
     if turn == 0:
         pick = int(input("{}, how many sticks you will take (1 or 2) : ".format(name)))
-        if pick == 1 or pick == 2 and status == 0:  # normal cases, user follows the rule
+        if pick != 1 or pick != 2:
+            status = 1
+        if (pick == 1 or pick == 2):  # normal cases, user follows the rule
             print(pick)
             stick -= pick
             turn = 1
-            bot = random.randint(1, 2)
             print("There is/are", stick, "stick(s) left in pile")
+        else:
+            print("You can pick only 1 or 2 stick!!")
+            status = 0
+            
     if turn == 1:
         if (stick + 2) % 3 == 1:
             print('I, smart computer, takes : 1 ')
@@ -35,4 +38,5 @@ while (stick > 0):
             stick -=bot
             print("There is/are", stick, "stick(s) left in pile")
             turn = 0
+    
     
