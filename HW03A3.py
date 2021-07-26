6cbf29469cbb646d1ea86c97fb5cef17c88164f2
Chomchanok <chomchanok_y@cmu.ac.th>
2021-07-26 20:38:40 +0700

    finish smart bot

--- HW03A3.py
+++ HW03A3.py
@@ -10,33 +10,39 @@ else:
 while (stick > 0):
     if turn == 0:
         pick = int(input("{}, how many sticks you will take (1 or 2) : ".format(name)))
-        if pick != 1 or pick != 2:
-            status = 1
         if (pick == 1 or pick == 2):  # normal cases, user follows the rule
-            print(pick)
             stick -= pick
             turn = 1
             print("There is/are", stick, "stick(s) left in pile")
+            if stick == 0:
+                print("You loose!!! try again in next game :)")
+                break
         else:
             print("You can pick only 1 or 2 stick!!")
-            status = 0
             
     if turn == 1:
-        if (stick + 2) % 3 == 1:
-            print('I, smart computer, takes : 1 ')
-            stick -= 1
-            print("There is/are", stick, "stick(s) left in pile")
-            turn = 0
-        if (stick + 2) % 3 == 2:
-            print('I, smart computer, takes : 2 ')
-            stick -= 2
-            print("There is/are", stick, "stick(s) left in pile")
-            turn = 0
-        if (stick + 2) % 3 == 0:
-            bot = random.randint(1, 2)
-            print('I, smart computer, takes : {}'.format(bot))
-            stick -=bot
-            print("There is/are", stick, "stick(s) left in pile")
-            turn = 0
+        
+            if (stick + 2) % 3 == 1 and stick != 1:
+                print('I, smart computer, takes : 1 ')
+                stick -= 1
+                print("There is/are", stick, "stick(s) left in pile")
+                turn = 0
+            elif (stick + 2) % 3 == 2 and stick != 1:
+                print('I, smart computer, takes : 2 ')
+                stick -= 2
+                print("There is/are", stick, "stick(s) left in pile")
+                turn = 0
+            elif (stick + 2) % 3 == 0 and stick != 1:
+                bot = random.randint(1, 2)
+                print('I, smart computer, takes : {}'.format(bot))
+                stick -=bot
+                print("There is/are", stick, "stick(s) left in pile")
+                turn = 0
+            else:
+                stick -= 1
+                print('I, smart computer, takes : 1 ')
+                print("There is/are", stick, "stick(s) left in pile")
+                print("You win :)")
+        
     
     
